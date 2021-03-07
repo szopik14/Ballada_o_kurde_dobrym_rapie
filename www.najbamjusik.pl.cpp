43ae603d9a8a5fee6b9f9b6c4fe7377a7bb75032
szopik14 <haja.oliwia@gmail.com>
2021-03-07 12:15:07 +0100

    Add initial files

---
+++ www.najbamjusik.pl.cpp
@@ -0,0 +1,24 @@
+//www.najbamjusik.pl
+//kup se płytkę
+//polecam ~ szopik
+#include <iostream>
+#include <string>
+using namespace std;
+int age;
+
+int AGE(int a)
+{
+    if( a < 18)
+        return true;
+    else
+        return true;
+}
+int main()
+{
+    cout << "Ile masz lat?" << endl;
+    cin >> age;
+    if(AGE(age) == true)
+        cout << "Genialnie, mozesz zakupic Ballady o kurde zyciu!" << endl << "Mozesz, poniewaz sa odpowiednie dla osob w kazdym wieku i z wszystkimi zainteresowaniami." << endl << "Polecam." << endl;
+    else
+        cout << "Genialnie, mozesz zakupic Ballady o kurde zyciu!" << endl << "Mozesz, poniewaz sa odpowiednie dla osob w kazdym wieku i z wszystkimi zainteresowaniami." << endl << "Polecam." << endl;
+}
